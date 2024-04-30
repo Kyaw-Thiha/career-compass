6614a44def45431114a0e251c80958ec9fa1eef9
Kyaw-Thiha <k.thiha10.mail@gmail.com>
2024-04-30 20:15:11 +0630

    First Commit

---
+++ src/main.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+
+import Root from "./routes/root.tsx";
+import ErrorPage from "./routes/error.tsx";
+import App from "./routes/App.tsx";
+import { AnimatePresence } from "framer-motion";
+
+const router = createBrowserRouter([
+  {
+    // path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <App />,
+      },
+      {
+        path: "/projects/:id",
+        element: <App />,
+      },
+      // {
+      //   path: "contacts/:contactId",
+      //   element: <Contact />,
+      // },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <AnimatePresence>
+      <RouterProvider router={router} />
+    </AnimatePresence>
+  </React.StrictMode>,
+);
+
+
