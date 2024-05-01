b30082768545f86701055a360cedc522b66141a8
Kyaw-Thiha <k.thiha10.mail@gmail.com>
2024-05-01 17:02:22 +0630

    Added Different Test Mentors

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -27,11 +27,11 @@ export const mentors = [
     },
     {
         id: 2,
-        name: "John",
-        position: "Head of Marketing",
-        description: "5 years of leading marketing teams",
-        image: "/mentors/albert-dera.jpg",
-        skills: ["Interview Preparation", "Goal Setting", "Team Management"],
+        name: "Aiony Haust",
+        position: "Software Engineer @ Google",
+        description: "5 years of software engineer experience in Fintech industry",
+        image: "/mentors/alony-haust.jpg",
+        skills: ["Interview Preparation", "Coding", "Team Work"],
         sessions: [
             {
                 text: "Career Development",
@@ -39,12 +39,12 @@ export const mentors = [
                 fee: 30000
             },
             {
-                text: "Resume Feedback",
+                text: "Code Review",
                 duration: 30,
                 fee: 50000
             },
             {
-                text: "Marketing Tips",
+                text: "Leadership",
                 duration: 60,
                 fee: 100000
             },
@@ -53,10 +53,10 @@ export const mentors = [
     },
     {
         id: 3,
-        name: "John",
-        position: "Head of Marketing",
-        description: "5 years of leading marketing teams",
-        image: "/mentors/albert-dera.jpg",
+        name: "Stefan Stefancik",
+        position: "Freelance Author",
+        description: "2 years of content writing",
+        image: "/mentors/stefan-stefancik.jpg",
         skills: ["Interview Preparation", "Goal Setting", "Team Management"],
         sessions: [
             {
@@ -79,10 +79,10 @@ export const mentors = [
     },
     {
         id: 4,
-        name: "John",
-        position: "Head of Marketing",
-        description: "5 years of leading marketing teams",
-        image: "/mentors/albert-dera.jpg",
+        name: "Tracy Pham",
+        position: "Data Science Manager",
+        description: "5 years experience",
+        image: "/mentors/jimmy-fermin.jpg",
         skills: ["Interview Preparation", "Goal Setting", "Team Management"],
         sessions: [
             {
@@ -105,10 +105,10 @@ export const mentors = [
     },
     {
         id: 5,
-        name: "John",
+        name: "Joseph Gonzalez",
         position: "Head of Marketing",
         description: "5 years of leading marketing teams",
-        image: "/mentors/albert-dera.jpg",
+        image: "/mentors/joseph-gonzalez.jpg",
         skills: ["Interview Preparation", "Goal Setting", "Team Management"],
         sessions: [
             {
